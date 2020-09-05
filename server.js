4de57ff89bbf94aac445c6d9c884766f20a506a3
MrMoshkovitz <gal@gmail.com>
2020-09-05 18:07:28 +0300

    Get Exercise

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app = express()
 const morgan = require("morgan");
 const getDate = require("./utils/getCurrentDateTime");
+const bodyParser = require('body-parser')
+
 morgan.token("body", function (req, res) {
 	return JSON.stringify(req.body);
 });
@@ -21,8 +23,9 @@ app.use(
 // const bodyParser = require('body-parser')
 
 const cors = require('cors')
-
+// app.use(express.json())
 const mongoose = require("mongoose");
+const { response } = require('express');
 let uri =`mongodb+srv://fullstack:${process.env.PW}@cluster0.wiesv.mongodb.net/${process.env.DBName}?retryWrites=true&w=majority`
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true },() => console.log(`Connecting to DB => ${process.env.DBName}`))
 
@@ -61,5 +64,34 @@ let userSchema = new mongoose.Schema({
   log: [exerciseSessionSchema]
 })
 
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.__v
+  }
+})
+
 let Session = mongoose.model('Session' , exerciseSessionSchema)
 let User = mongoose.model('User', userSchema)
+
+app.post('/api/exercise/new-user', bodyParser.urlencoded({ extended: false }), (req, res) => {
+  let newUser = new User({username: req.body.username})
+  newUser.save((err, savedUser) => {
+    if(!err) {
+      let responseObject = {}
+      responseObject = {
+        _id : savedUser.id,
+        username: savedUser.username
+      }
+      res.json(responseObject)
+    }
+  })
+})
+
+app.get('/api/exercise/users', (req, res) => {
+
+  User.find({}, (err, usersArr) => {
+    if(!err) {
+      res.json(usersArr)
+    }
+  })
+})
